116cfd63ae541980366225d958c6dee218e61c63
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-20 10:24:26 +0000

    Deploy SPE-R/SPE-R.github.io to github.com/SPE-R/SPE-R.github.io.git:travis-build

--- Rsolutions/causal-s.R
+++ Rsolutions/causal-s.R
@@ -1,7 +1,26 @@
 ### R code from vignette source '/home/travis/build/SPE-R/SPE/build/causal-s.rnw'
 
 ###################################################
-### code chunk number 1: beerdata_1
+### code chunk number 1: causgraph1
+###################################################
+library(Epi)
+par( mar=c(0,0,0,0), cex=2)
+plot( NA, bty="n",xlim= c(40,100), ylim=c(0,80), xaxt="n", yaxt="n",
+ xlab="", ylab="" ) # create an empty plot with coordinates
+b<-0; w=12
+bb  <- tbox( "beer", 44, 40, w,w, col.txt="red" ,col.border=b)
+ww  <- tbox( "weight", 90, 40, w,w, col.txt="red" ,col.border=b)
+ss  <- tbox( "sex", 67, 70, w,w, col.txt="blue" ,col.border=b)
+bp  <- tbox( "BP", 67, 10, w,w, col.txt="blue" ,col.border=b)
+text( boxarr( bb, ww , col="red", lwd=3 ,gap= 4), "?", col="red", adj=c(0,-0.5) )
+boxarr( bb, bp , col="blue", lwd=3 )
+boxarr( ww, bp , col="blue", lwd=3 )
+boxarr( ss, bb , col="blue", lwd=3 )
+boxarr( ss, ww , col="blue", lwd=3 )
+
+
+###################################################
+### code chunk number 2: beerdata_1
 ###################################################
 set.seed(02062017)
 bdat= data.frame(sex = c(rep(0,500),rep(1,500))  )
@@ -12,7 +31,7 @@ bdat$bp <- 110 + 0.5*bdat$weight + 10*bdat$beer + rnorm(1000,0,10)
 
 
 ###################################################
-### code chunk number 2: beermodels_1
+### code chunk number 3: beermodels_1
 ###################################################
 library( Epi )
 m1a<-lm(weight~beer, data=bdat)
@@ -24,13 +43,13 @@ ci.lin(m3a)
 
 
 ###################################################
-### code chunk number 3: beerdata_2
+### code chunk number 4: beerdata_2
 ###################################################
 bdat$weight <- 60 + 10*bdat$sex + 2*bdat$beer + rnorm(1000,0,7)
 
 
 ###################################################
-### code chunk number 4: beermodels_2b
+### code chunk number 5: beermodels_2b
 ###################################################
 bdat$bp <- 110 +0.5*bdat$weight  + 10*bdat$beer+ rnorm(1000,0,10)  #
 m1b<-lm(weight~beer,data=bdat)
@@ -42,17 +61,18 @@ ci.lin(m3b)
 
 
 ###################################################
-### code chunk number 5: bpmodel
+### code chunk number 6: bpmodel
 ###################################################
 m1bp<-lm(bp~beer,data=bdat)
-m2bp<-lm(bp~beer+weight+sex,data=bdat)
-m3b<-lm(weight~beer+sex+bp,data=bdat)
+m2bp<-lm(bp~beer+weight,data=bdat)
+m3bp<-lm(bp~beer+weight+sex,data=bdat)
 ci.lin(m1bp)
 ci.lin(m2bp)    # the correct model
+ci.lin(m3bp)    # also correct!
 
 
 ###################################################
-### code chunk number 6: mrdat1
+### code chunk number 7: mrdat1
 ###################################################
  n <- 10000
  mrdat <- data.frame(G = rbinom(n,2,0.2))
@@ -60,39 +80,39 @@ ci.lin(m2bp)    # the correct model
 
 
 ###################################################
-### code chunk number 7: mrdat2
+### code chunk number 8: mrdat2
 ###################################################
 mrdat$U <- rnorm(n)
 
 
 ###################################################
-### code chunk number 8: mrdat3
+### code chunk number 9: mrdat3
 ###################################################
 mrdat$BMI <- with(mrdat, 25 + 0.7*G + 2*U + rnorm(n) )
 
 
 ###################################################
-### code chunk number 9: mrdat4
+### code chunk number 10: mrdat4
 ###################################################
 mrdat$Y <- with(mrdat, 3 + 0.1*BMI - 1.5*U + rnorm(n,0,0.5) )
 
 
 ###################################################
-### code chunk number 10: mrmod1
+### code chunk number 11: mrmod1
 ###################################################
 mxy<-lm(Y ~ BMI, data=mrdat)
 ci.lin(mxy)
 
 
 ###################################################
-### code chunk number 11: mrmod2
+### code chunk number 12: mrmod2
 ###################################################
 mxyg<-lm(Y ~ G + BMI, data=mrdat)
 ci.lin(mxyg)
 
 
 ###################################################
-### code chunk number 12: mrmod3
+### code chunk number 13: mrmod3
 ###################################################
 mgx<-lm(BMI ~ G, data=mrdat)
 ci.lin(mgx)  # check the instrument effect
@@ -105,7 +125,7 @@ causeff    # closer to 0.1?
 
 
 ###################################################
-### code chunk number 13: mrsim
+### code chunk number 14: mrsim
 ###################################################
 n <- 10000
 # initializing simulations:
@@ -128,19 +148,19 @@ mr[i]<-bgy/bgx
 
 
 ###################################################
-### code chunk number 14: mrsim2
+### code chunk number 15: mrsim2
 ###################################################
 summary(mr)    
 
 
 ###################################################
-### code chunk number 15: mrsim3
+### code chunk number 16: mrsim3
 ###################################################
 mrdat$Y <- with(mrdat, 3 + 0.1*BMI - 1.5*U + 0.05*G + rnorm(n,0,0.5) )
 
 
 ###################################################
-### code chunk number 16: tsls
+### code chunk number 17: tsls
 ###################################################
 library(sem)
 summary(tsls(Y ~ BMI, ~G, data=mrdat))
