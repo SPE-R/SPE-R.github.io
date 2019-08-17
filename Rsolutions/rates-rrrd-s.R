5d2b6ff7dd2a53799bc14651b0a3d71302715147
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-17 08:08:57 +0000

    Deploy SPE-R/SPE-R.github.io to github.com/SPE-R/SPE-R.github.io.git:travis-build

--- Rsolutions/rates-rrrd-s.R
+++ Rsolutions/rates-rrrd-s.R
@@ -1,24 +1,24 @@
 ### R code from vignette source '/home/travis/build/SPE-R/SPE/build/rates-rrrd-s.rnw'
 
 ###################################################
-### code chunk number 1: rates-rrrd-s.rnw:48-50
+### code chunk number 1: rates-rrrd-s.rnw:47-49
 ###################################################
 library( Epi )
-options(digits=4)  # cut the number of decimals in the output
+options(digits=4)  #  to cut down decimal points in the output
 
 
 ###################################################
-### code chunk number 2: rates-rrrd-s.rnw:52-57
+### code chunk number 2: rates-rrrd-s.rnw:51-56
 ###################################################
 D <- 15
-Y <- 5.532    # thousands of years
+Y <- 5.532    # thousands of years!
 rate <- D / Y
 SE.rate <- rate/sqrt(D)
 c(rate, SE.rate, rate + c(-1.96, 1.96)*SE.rate )
 
 
 ###################################################
-### code chunk number 3: rates-rrrd-s.rnw:62-66
+### code chunk number 3: rates-rrrd-s.rnw:61-65
 ###################################################
 SE.logr <- 1/sqrt(D)
 EF <- exp( 1.96 * SE.logr )
@@ -27,61 +27,55 @@ c( rate, EF, rate/EF, rate*EF )
 
 
 ###################################################
-### code chunk number 4: rates-rrrd-s.rnw:81-83
+### code chunk number 4: rates-rrrd-s.rnw:88-90
 ###################################################
 m <- glm( D ~ 1, family=poisson(link=log), offset=log(Y) )
 summary( m )
 
 
 ###################################################
-### code chunk number 5: rates-rrrd-s.rnw:92-93
+### code chunk number 5: rates-rrrd-s.rnw:101-102
 ###################################################
 ci.lin( m )
 
 
 ###################################################
-### code chunk number 6: rates-rrrd-s.rnw:98-99
+### code chunk number 6: rates-rrrd-s.rnw:107-108
 ###################################################
-ci.lin( m, Exp=TRUE )
+ci.lin( m, Exp=TRUE)
 
 
 ###################################################
-### code chunk number 7: rates-rrrd-s.rnw:104-106
+### code chunk number 7: rates-rrrd-s.rnw:113-115
 ###################################################
-ci.exp( m )
-ci.exp( m, pval=TRUE )
+ci.exp( m)
+ci.lin( m, Exp=TRUE)[, 5:7] 
 
 
 ###################################################
-### code chunk number 8: rates-rrrd-s.rnw:117-119
+### code chunk number 8: rates-rrrd-s.rnw:128-130
 ###################################################
-mw <- glm( D/Y ~ 1, family=poisson, weight=Y )
-ci.exp( mw, pval=T ) 
+mreg <- glm( cbind(D, Y) ~ 1, family=poisreg(link=log) )
+ci.exp( mreg ) 
 
 
 ###################################################
-### code chunk number 9: rates-rrrd-s.rnw:132-134
+### code chunk number 9: rates-rrrd-s.rnw:147-150
 ###################################################
-mi <- glm( D/Y ~ 1, family=poisson(link=identity), weight=Y )
-coef( mi )
+mid <- glm( cbind(D,Y) ~ 1, family=poisreg(link=identity) )
+ci.lin( mid )
+ci.lin( mid )[, c(1,5,6)]
 
 
 ###################################################
-### code chunk number 10: rates-rrrd-s.rnw:142-144
+### code chunk number 10: rates-rrrd-s.rnw:175-177
 ###################################################
-ci.lin( mi )
-ci.lin( mi )[, c(1,5,6)]
-
-
-###################################################
-### code chunk number 11: rates-rrrd-s.rnw:165-167
-###################################################
-ci.lin( mi )
+ci.lin( mid )
 sqrt(D)/Y 
 
 
 ###################################################
-### code chunk number 12: rates-rrrd-s.rnw:179-184
+### code chunk number 11: rates-rrrd-s.rnw:189-194
 ###################################################
 Dx <- c(3,7,5)
 Yx <- c(1.412,2.783,1.337)
@@ -91,75 +85,83 @@ rates
 
 
 ###################################################
-### code chunk number 13: rates-rrrd-s.rnw:189-191
+### code chunk number 12: rates-rrrd-s.rnw:199-201
 ###################################################
-m3 <- glm( Dx ~ 1, family=poisson, offset=log(Yx)  )
+m3 <- glm( cbind(Dx,Yx) ~ 1, family=poisreg(link=log) )
 ci.exp( m3 )
 
 
 ###################################################
-### code chunk number 14: rates-rrrd-s.rnw:196-197
+### code chunk number 13: rates-rrrd-s.rnw:206-208
 ###################################################
-mp <- glm( Dx ~ factor(Px), offset=log(Yx), family=poisson )
+mp <- glm( cbind(Dx,Yx) ~ factor(Px), family=poisreg(link=log) )
+ci.exp(mp)
 
 
 ###################################################
-### code chunk number 15: rates-rrrd-s.rnw:201-203
+### code chunk number 14: rates-rrrd-s.rnw:213-214
 ###################################################
-options( digits=7 )
 anova( m3, mp, test="Chisq" )
 
 
 ###################################################
-### code chunk number 16: rates-rrrd-s.rnw:247-253
+### code chunk number 15: rates-rrrd-s.rnw:267-274
 ###################################################
 D0 <- 15   ; D1 <- 28
 Y0 <- 5.532 ; Y1 <- 4.783
-RR <- (D1/Y1)/(D0/Y0)
+R1 <- D1/Y1; R0 <- D0/Y0
+RR <- R1/R0
 SE.lrr <- sqrt(1/D0+1/D1) 
 EF <- exp( 1.96 * SE.lrr)
-c( RR, RR/EF, RR*EF )
+c( R1, R0, RR, RR/EF, RR*EF )
 
 
 ###################################################
-### code chunk number 17: rates-rrrd-s.rnw:257-259
+### code chunk number 16: rates-rrrd-s.rnw:280-282
 ###################################################
 D <- c(D0,D1) ; Y <- c(Y0,Y1); expos <- 0:1
-mm <- glm( D ~ factor(expos), family=poisson, offset=log(Y) )
+mm <- glm( cbind(D,Y) ~ factor(expos), family=poisreg(link=log) )
 
 
 ###################################################
-### code chunk number 18: rates-rrrd-s.rnw:264-266
+### code chunk number 17: rates-rrrd-s.rnw:287-289
 ###################################################
-ci.exp( mm)
-ci.lin( mm, E=T)[,5:7]
+ci.exp( mm )
+ci.lin( mm, Exp=TRUE ) [,5:7]
 
 
 ###################################################
-### code chunk number 19: rates-rrrd-s.rnw:286-289
+### code chunk number 18: rates-rrrd-s.rnw:309-312
 ###################################################
-rd <- diff( D/Y )
-sed <- sqrt( sum( D/Y^2 ) )
-c( rd, rd+c(-1,1)*1.96*sed )
+RD <- diff( D/Y )    ##  or RD <- R1 - R0
+SED <- sqrt( sum( D/Y^2 ) )
+c( R1, R0, RD, SED, RD+c(-1,1)*1.96*SED )
 
 
 ###################################################
-### code chunk number 20: rates-rrrd-s.rnw:293-296
+### code chunk number 19: rates-rrrd-s.rnw:316-319
 ###################################################
-ma <- glm( D/Y ~ factor(expos), 
-          family=poisson(link=identity), weight=Y )
+ma <- glm( cbind(D,Y) ~ factor(expos), 
+           family=poisreg(link=identity) )
 ci.lin( ma )[, c(1,5,6)]
 
 
 ###################################################
-### code chunk number 21: rates-rrrd-s.rnw:311-313
+### code chunk number 20: rates-rrrd-s.rnw:337-339
+###################################################
+mwei <- glm( D/Y ~ 1, family=poisson(link=log), weight=Y )
+ci.exp( mwei ) 
+
+
+###################################################
+### code chunk number 21: rates-rrrd-s.rnw:362-364
 ###################################################
 ci.mat
 ci.mat()
 
 
 ###################################################
-### code chunk number 22: rates-rrrd-s.rnw:324-327
+### code chunk number 22: rates-rrrd-s.rnw:375-378
 ###################################################
 rateandSE <- c( rate, SE.rate ) 
 rateandSE
@@ -167,7 +169,7 @@ rateandSE %*% ci.mat()
 
 
 ###################################################
-### code chunk number 23: rates-rrrd-s.rnw:333-336
+### code chunk number 23: rates-rrrd-s.rnw:383-386
 ###################################################
 lograndSE <- c( log(rate), SE.logr )
 lograndSE
@@ -175,35 +177,35 @@ exp( lograndSE %*% ci.mat() )
 
 
 ###################################################
-### code chunk number 24: rates-rrrd-s.rnw:341-342
+### code chunk number 24: rates-rrrd-s.rnw:391-392
 ###################################################
 exp( c( log(RR), SE.lrr ) %*% ci.mat() )
 
 
 ###################################################
-### code chunk number 25: rates-rrrd-s.rnw:349-351
+### code chunk number 25: rates-rrrd-s.rnw:400-402
 ###################################################
-ci.mat(alpha=0.1)
+ci.mat( alpha=0.1 )
 exp( c( log(RR), SE.lrr ) %*% ci.mat(alpha=0.1) )
 
 
 ###################################################
-### code chunk number 26: rates-rrrd-s.rnw:359-365
+### code chunk number 26: rates-rrrd-s.rnw:409-415
 ###################################################
 CM <- rbind( c(1,0), c(1,1), c(0,1) )
 rownames( CM ) <- c("rate 0","rate 1","RR 1 vs. 0")
 CM
 mm <- glm( D ~ factor(expos),
-              family=poisson(link=log),  offset=log(Y) )
-ci.exp( mm, ctr.mat=CM)
+           family=poisson(link=log),  offset=log(Y) )
+ci.exp( mm, ctr.mat=CM )
 
 
 ###################################################
-### code chunk number 27: rates-rrrd-s.rnw:370-374
+### code chunk number 27: rates-rrrd-s.rnw:420-424
 ###################################################
 rownames( CM ) <- c("rate 0","rate 1","RD 1 vs. 0")
-ma <- glm( D/Y ~ factor(expos),
-                 family=poisson(link=identity), weight=Y )
+ma <- glm( cbind(D,Y) ~ factor(expos),
+                 family=poisreg(link=identity) )
 ci.lin( ma, ctr.mat=CM )[, c(1,5,6)]
 
 
