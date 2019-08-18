94c08c0ed16ac556db0ed0f0e56da42de2c7e9ac
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-18 12:18:04 +0000

    Deploy SPE-R/SPE-R.github.io to github.com/SPE-R/SPE-R.github.io.git:travis-build

--- Rsolutions/graphics-s.R
+++ Rsolutions/graphics-s.R
@@ -34,7 +34,7 @@ means <- as.matrix(means[-1])
 ###################################################
 ### code chunk number 5: graphics-s.rnw:36-38
 ###################################################
-time <- c(0,3,6,9,12,18,24)
+times <- c(0,3,6,9,12,18,24)
 (ylim <- range(means+sems,means-sems))
 
 
@@ -42,40 +42,40 @@ time <- c(0,3,6,9,12,18,24)
 ### code chunk number 6: alkfos
 ###################################################
 par(mar=.1 + c(8,4,4,2))
-plot( time, means[1,],
+plot( times, means[1,],
       type="b",   # boths dots and lines
       xaxt="n",   # no x-axis
       ylim=ylim,
       ylab="alkaline phosphatase" )
 # Add the points for the second gropu
-points(time,means[2,], type="b")
+points(times,means[2,], type="b")
 # The the vertial error-bars
-segments( time, upr[1,], time, lwr[1,] )
-segments( time, upr[2,], time, lwr[2,])
+segments( times, upr[1,], times, lwr[1,] )
+segments(times, upr[2,], times, lwr[2,])
 # Draw the x-axis at the times
-axis( 1, at=time )
+axis(1, at=times )
 # Plot the availabe numbers below the x-axis
-mtext( available[1,], side=1, line=5, at=time)
-mtext( available[2,], side=1, line=6, at=time)
+mtext(available[1,], side=1, line=5, at=times)
+mtext(available[2,], side=1, line=6, at=times)
 
 
 ###################################################
 ### code chunk number 7: alkfos-x
 ###################################################
 par(mar=.1 + c(8,5,4,2))
-plot(time,means[1,], type="b", ylim=ylim, xaxt="n",
+plot(times, means[1,], type="b", ylim=ylim, xaxt="n",
                      ylab="% change in alkaline phosphatase",
                      xlab="Months after randomization",
                      las=1, # All axis labels horizontal
                      pch=16, # Dot as plotting symbol
                      bty="n", lwd=2 ) # No box around the plot
-segments(time ,  upr[1,], time , lwr[1,], lwd=2 )
-time2 <- time + 0.25 # Plot the points and bars a little bit offset
-points  (time2,means[2,], type="b", pch=18, lwd=2, cex=1.3)
-segments(time2,  upr[2,], time2, lwr[2,], lwd=2 )
-axis(1,at=time)
-mtext(available[1,],side=1, line=5, at=time)
-mtext(available[2,],side=1, line=6, at=time)
+segments(times,  upr[1,], times , lwr[1,], lwd=2 )
+times2 <- times + 0.25 # Plot the points and bars a little bit offset
+points  (times2, means[2,], type="b", pch=18, lwd=2, cex=1.3)
+segments(times2, upr[2,], times2, lwr[2,], lwd=2 )
+axis(1,at=times)
+mtext(available[1,],side=1, line=5, at=times)
+mtext(available[2,],side=1, line=6, at=times)
 # par("usr") reaturns the actual x- and y coordinates of the axis ends.
 mtext("Placebo"  , side=1, line=5, adj=1, at=par("usr")[1] )
 mtext("Tamoxifen", side=1, line=6, adj=1, at=par("usr")[1] )
